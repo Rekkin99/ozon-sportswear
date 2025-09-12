6488012f782ebc1aa3b00fd5816ca04759459e1c
Rekkin99 <farrellbagoes04@gmail.com>
2025-09-13 00:04:02 +0700

    (9) Added Form Page and Product Detail Page

--- main/views.py
+++ main/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import Catalogue
+from main.models import Product
 
 # Create your views here.
 def show_main(request):
+    # Return All Product Objects in Database
+    product_list = Product.objects.all()
+
     context = {
         'app' : 'Ozon Sportswear',
         'name': 'Farrell Bagoes Rahmantyo',
         'npm' : '2406420596',
+        'product_list' : product_list 
     }
 
     return render(request, "main.html", context)
+
+def add_product(request):
+    form = Catalogue(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "add_product.html", context)
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    
+
+    context = {
+        'product': product,
+        'price' : "Rp. {:,}".format(product.price)
+    }
+
+    return render(request, "product_detail.html", context)
--- ozon_sportswear/settings.py
+++ ozon_sportswear/settings.py
@@ -32,6 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "farrell-bagoes-ozonsportswear.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://farrell-bagoes-ozonsportswear.pbp.cs.ui.ac.id/",
+]
+
 
 # Application definition
 
